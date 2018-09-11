cd4928be1a5ca65e8539089bbb734b64a53ad6f2
blackdeath1098 <simonzhang901@gmail.com>
2018-09-10 21:41:43 -0400

    Semi-finished product

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+        // Transition effect for navbar
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 635) {
+              $('nav').addClass('solid');
+              console.log("this script works");
+          } else {
+              $('nav').removeClass('solid');
+          }
+        });
+});
