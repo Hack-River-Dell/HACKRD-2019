9cc3a328f13d7620940d8040eef70145cac0ee9f
blackdeath1098 <simonzhang901@gmail.com>
2018-09-15 18:02:52 -0400

    added table and tabs

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,24 @@ $(document).ready(function() {
           // checks if window is scrolled more than 500px, adds/removes solid class
           if($(this).scrollTop() > 635) {
               $('nav').addClass('solid');
-              console.log("this script works");
+
           } else {
               $('nav').removeClass('solid');
           }
         });
 
-        
 
 });
+function changeDate(evt, dayOfHack) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(dayOfHack).style.display = "block";
+    evt.currentTarget.dayOfHack += " active";
+}
